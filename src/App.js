e9f9a6459dfccf026c77a38565a8f4292f4dd472
maiYASHhoon <yash280801@gmail.com>
2023-06-27 23:50:07 +0530

    css

--- src/App.js
+++ src/App.js
@@ -6,29 +6,40 @@ import Profile from "./components/profile";
 const App = () => {
     return (
         <BrowserRouter>
-            <header>
-                <Link to="">Home</Link>
-                <br />
-                <Link to="posts">Posts</Link>
-                <br />
-                <Link
-                    to={{
-                        pathname: "profile",
-                        hash: "#yash",
-                        search: "?profile=true",
-                    }}
-                >
-                    Profile
-                </Link>
-                <br />
-                <br />
-                <hr />
-            </header>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="posts" element={<Post />} />
-                <Route path="profile" element={<Profile />} />
-            </Routes>
+            <div className="container">
+                <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+                    <Link
+                        to="/"
+                        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+                    >
+                        <span className="fs-4">My app</span>
+                    </Link>
+
+                    <ul className="nav nav-pills">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="posts" className="nav-link">
+                                Posts
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="profile" className="nav-link">
+                                Profile
+                            </Link>
+                        </li>
+                    </ul>
+                </header>
+
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="posts" element={<Post />}></Route>
+                    <Route path="profile" element={<Profile />}></Route>
+                </Routes>
+            </div>
         </BrowserRouter>
     );
 };
